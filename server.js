e328ad775ecdc7fc202b0a83838a5a829add87ba
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-04-02 18:49:12 +0530

    javascript 1st day

---
+++ server.js
@@ -0,0 +1,62 @@
+// var a = 676868;
+// console.log(a)
+// console.log(typeof(a))
+
+// var b = "hello";
+// console.log(b)
+// console.log(typeof(b))
+
+// var c = true;
+// console.log(c)
+// console.log(typeof(c))
+
+// var d = false;
+// console.log(d)
+// console.log(typeof(d))
+
+// var d = '676868';
+// console.log(d)
+// console.log(typeof(d))
+
+// var e;
+// console.log(e)
+// console.log(typeof(e))
+
+// var f = '&&%';
+// console.log(f)
+// console.log(typeof(f))
+
+
+
+
+// var a = 768778;
+// console.log(a)
+
+// var b = 7687;
+// console.log(b)
+
+// console.log(a+b)
+// console.log(a-b)
+
+
+// var a = 76757;
+// console.log(a)
+
+// var b = "9878768767";
+// console.log(b)
+
+// console.log(a+b)
+
+// var a = 7777;
+
+// var b = "7777";
+
+// console.log(a == b)     check data only
+// console.log(a === b)    check data and data types
+
+
+var a = 9898;
+var b = 9876;
+
+console.log(a && b)
+console.log(a || b)
