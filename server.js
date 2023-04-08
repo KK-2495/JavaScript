c73c46394d7a1d4bdbfe442a05c246d6a0fd74b6
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-04-08 15:16:56 +0530

    updates

--- server.js
+++ server.js
@@ -96,22 +96,123 @@
 
 // console.log("Welcome to RTO")
 
-var userAge = 47;
-var flagForSwitch = true
-switch (flagForSwitch) {
-    case (userAge > 18):
-        console.log("You are eligible for DL");
-        flagForSwitch = false;
-        break;
-    case (userAge < 18):
-        console.log("You are not eligible for DL");
-        flagForSwitch = false;
-        break;
-    case (userAge <= 10):
-        console.log("You are a kid");
-        flagForSwitch = false;
-        break;
-    default:
-        console.log("Invalid AGE");
-        flagForSwitch = false;
-} 
+// var userAge = 47;
+// var flagForSwitch = true
+// switch (flagForSwitch) {
+//     case (userAge > 18):
+//         console.log("You are eligible for DL");
+//         flagForSwitch = false;
+//         break;
+//     case (userAge < 18):
+//         console.log("You are not eligible for DL");
+//         flagForSwitch = false;
+//         break;
+//     case (userAge <= 10):
+//         console.log("You are a kid");
+//         flagForSwitch = false;
+//         break;
+//     default:
+//         console.log("Invalid AGE");
+//         flagForSwitch = false;
+// } 
+//TEST//
+// console.log("Checking the number is Odd or Even");
+
+// var num = 233;
+// if (num % 2 == 0){
+//     console.log("it is even");
+// }else{
+//     console.log("it is odd");
+// }
+
+// test2//
+// console.log("Find the largest of two number.");
+// var num1 = 5;
+// var num2 = 10;
+
+// if (num1 > num2){
+//     console.log("this is the largest number");
+// } else {
+//     console.log("this is not the largest nmber");
+// }
+
+// console.log("Find the largest of three number.");
+// var num1 = 65;
+// var num2 = 50;
+// var num3 = 40;
+
+// if (num1 > num2 && num1 > num3){
+//     console.log("num1 is the largest number");
+// } 
+// else if (num2 > num3){
+//     console.log("num2 is the largest nmber");
+// }
+// else {
+//     console.log("num3 is the largest number");
+// }
+
+// console.log("Find a number is present in given range.");
+// var firstRange = 5;
+// var secondRange = 25;
+// var number = 1;
+// if(number >= 5 && number <= 25){
+//     console.log("This falls under the range");
+// }else {
+//     console.log("this does not fall under the range");
+// }
+// Test using switch case //
+// console.log("Checking the number is Odd or Even");
+// var num = 232;
+// var flagForSwitch = true;
+// switch (flagForSwitch) {
+//     case (num % 2 == 0):
+//         console.log("This is even");
+//         break;
+//     case (num % 2 != 0):
+//         console.log("This is odd");
+//         break;
+//     default:
+//         break;
+// }
+// console.log("Find the largest of two number.");
+// var num1 = 10;
+// var num2 = 20;
+// var number = true;
+// switch (number) {
+//     case (num1 > num2):
+//         console.log("num1 is greater")
+//         break;
+//     case (num2 > num1):
+//         console.log("num2 is greater")
+//         break;
+//     default:
+//         console.log("Invalid number")
+// }
+// console.log("Find the largest of three number.");
+// var num1 = 50;
+// var num2 = 30;
+// var num3 = 60;
+// var numbers = true;
+
+// switch (numbers) {
+//     case (num1>num2 && num1>num3):
+//         console.log("num1 is greater")    
+//         break;
+//         case (num2>num3):
+//         console.log("num2 is greater")    
+//         break;
+//     default:
+//         console.log("num is greater");
+// }
+// console.log("Find the number is present in given range.");
+
+// var firstRange = 5;
+// var secondRange = 25;
+// var range = 1
+// switch (true) {
+//     case (range > firstRange && range<25):
+//         console.log("This is under the given range");
+//         break;
+//     default:
+//         console.log("This does not fall under given range");
+// }
