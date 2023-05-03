77119a0d695245c0f76da1b7e82e6575fabd99cd
KK-2495 <krishnankalimuthunadar@hotmail.com>
2023-05-03 10:23:55 +0530

    updatee

---
+++ Practie session/30th April test/Krishna.js
@@ -0,0 +1,104 @@
+// 30th April
+// Q1
+// var arr = [1,2,3,4,5,6,7];
+// var input = 7;
+
+// function sum(){
+// for(var i=0; i<arr.length; i++){
+//     if(arr[i]%7==0){
+//         // console.log(arr[i]);
+//         arr[i] = arr
+//     }
+// } return arr;
+// }
+// var ans = sum();
+// console.log(ans);
+
+// Q2
+// var mat = [[1,0,1,0,0],[0,1,1,1,0]];
+// var newmat =[];
+
+// for(var i=0; i<mat.length; i++){
+//     var mat1 = mat[i];
+//     for(var j=mat1; j<mat1.length; j++){
+//         if(mat1==1){
+//             newmat.push(mat1[j]);
+//         }
+//     }
+// }
+// console.log(newmat);
+
+
+// var arr1 =[1,0,1,0,0];
+// var arrnew = [];
+// for(var i=0; i<arr1.length;i++){
+//     if(arr1[i]==1){
+//         arrnew.push(i);
+//     }
+// }
+// console.log(arrnew);
+
+
+// Q3
+// var nums = [[1,2,3],[5,6,7],[9,10,11]];
+// for(var i=0; i<nums.length; i++){
+//     var num = nums[i];
+//     for(var j=num+1; j<num.length; j++){
+//         if(num%num==0){
+//             num[j] = num;
+//         }
+//     }
+//         console.log(num);
+// }
+
+// Q4
+// var nums1 = [4,1,3];
+// var nums2 = [5,7];
+// // var newnums;
+// function least(a,b){
+//     for(var i=0; i<a.length; i++){
+//         for(var j=0; j<b.length; j++){
+//             if(a[i]<a[i+1] && b[j]<b[j+1]){
+//                 console.log(a[i], b[j]);
+//             }
+//         }
+//     }
+// }
+// var ans = least(nums1, nums2);
+// console.log(ans);
+
+// Q5
+// var s = "Hello how are you Contestant";
+// var k = 4;
+// var ss;
+
+// for(var i=0; i<s.length; i++){
+//     console.log(i);
+// }
+// console.log(ss);
+
+// Q6
+// var nums = [1,1,0,1,1,1];
+
+// for(var i=0; i<nums.length; i++){
+//     for(var j=nums.length-1; j>i; j--){
+//         if(nums[i]==1 && nums[j]==1){
+//             console.log(i,j);
+//         }
+//     }
+// }
+
+
+// Q7
+
+// var image = [[1,1,0],[1,0,1],[0,0,0]];
+
+// for(var i=0; i<image.length; i++){
+//     var newimage = image[i];
+//     var newimg = [];
+//     for(var j=newimage.length-1; j>=i; j--){
+//         newimg.push(newimage[j]);
+//     }
+//     console.log(newimg);
+
+// }
